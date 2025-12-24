d2503df9ca33540b6373dbc9b4b4e9e7e09d9a67
Alexis Sahagun <alexissahagunitj7a@gmail.com>
2025-12-24 16:50:41 -0600

    updted supabase proxy

--- lib/supabase/proxy.ts
+++ lib/supabase/proxy.ts
@@ -56,7 +56,10 @@ export async function updateSession(request: NextRequest) {
     !request.nextUrl.pathname.startsWith("/scan") &&
     !request.nextUrl.pathname.startsWith("/api/pass") &&
     !request.nextUrl.pathname.startsWith("/api/wallet") &&
-    !request.nextUrl.pathname.startsWith("/api/test")
+    !request.nextUrl.pathname.startsWith("/api/test") &&
+    !request.nextUrl.pathname.startsWith("/api/scan") &&
+    !request.nextUrl.pathname.startsWith("/api/redeem") &&
+    !request.nextUrl.pathname.startsWith("/api/purchase") &&
   ) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone();
